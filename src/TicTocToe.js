f156e63d7247ed71634ef378ffba4b6688983216
unknown <you@example.com>
2022-03-08 01:56:40 +0500

    second commit

--- src/TicTocToe.js
+++ src/TicTocToe.js
@@ -1,55 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const TicTocToe = () => {
-  const [turns, setTurns] = useState("X");
-  const [cells, setCells] = useState(Array(9).fill(""));
-  const [winner, setWinner] = useState();
+  const [cells, setcells] = useState(new Array(9).fill(null));
+  const [winning, setWinning] = useState(null);
 
-  
-  const ClickHandle = (num) => {
-    let chance = [...cells];
+     console.log(cells , "cells")
+     console.log(winning , "winning")
 
-    if (turns === "X") {
-      chance[num] = "X";
-      setTurns("O");
-    } else {
-      chance[num] = "O";
-      setTurns("X");
-    }
-    console.log(chance, "chance");
-    Winner(chance);
-    setCells(chance);
-  };
+  useEffect(() => {
+    const ComputerTurn = cells.filter((cell) => cell !== null).length % 2 === 1;
+    const EmptyCells = cells.map((cell, index) => cell === null ? index : null);
 
-  const Winner = (chance) => {
-    let lines = {
-      Line: [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-      ],
+    const ComputerMove = (num) => {
+      let newcells = cells;
+      newcells[num] = "O";
+      setcells([...newcells]);
     };
 
-    for (let line in lines) {
-      lines[line].forEach((pattern) => {
-        if (
-          chance[pattern[0]] === chance[pattern[1]] &&
-          chance[pattern[1]] === chance[pattern[2]]
-        ) {
-          setWinner(chance[pattern[0]]);
-        }
-      });
+    if (ComputerTurn) {
+      const random = EmptyCells[Math.ceil(Math.random() * EmptyCells.length)];
+      console.log(random , "random")
+      ComputerMove(random);
+    }
+    const Win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < Win.length; i++) {
+      const [a, b, c] = Win[i];
+      if (cells[a] && cells[a] === cells[b] && cells[b] === cells[c]) {
+        return setWinning(cells[a]);
+      }
+    }
+  }, [cells]);
+
+  const ClickHandle = (num) => {
+    const PlayerTurn = cells.filter((cell) => cell !== null).length % 2 === 0;
+    if (PlayerTurn) {
+      let newcells = cells;
+      newcells[num] = "X";
+      setcells([...newcells]);
     }
   };
 
-  const handleRestart = () => {
-    setWinner(null);
-    setCells(Array(9).fill(""));
+  const Restart = () => {
+    setWinning(null);
+    setcells(Array(9).fill(null));
   };
 
   const Cells = ({ num }) => {
@@ -59,9 +62,9 @@ const TicTocToe = () => {
   return (
     <div>
       <h2> Tic Toc Toe Game </h2>
-      Turn: {turns}
+
       <div className="container">
-        <table>
+        <table cellspacing="0">
           <tbody>
             <tr>
               <Cells num={0} />
@@ -81,10 +84,10 @@ const TicTocToe = () => {
           </tbody>
         </table>
       </div>
-      {winner && (
+      {winning && (
         <>
-          <p>{winner} is the winner!</p>
-          <button onClick={() => handleRestart()}>Play Again</button>
+          <h3>{winning} is the Winner</h3> 
+          <button onClick={() => Restart()}> Restart Game </button>
         </>
       )}
     </div>
